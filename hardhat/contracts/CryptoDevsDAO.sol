a4163baba54e85f401ee2f447bbaab9e5dbd228b
ranjankjha17 <talk2ranjanktr@gmail.com>
2022-05-29 10:22:36 +0530

    add contract

--- hardhat/contracts/CryptoDevsDAO.sol
+++ hardhat/contracts/CryptoDevsDAO.sol
@@ -21,4 +21,39 @@ interface ICryptoDevsNFT{
 
 contract CryptoDevsDAO is Ownable{
 
+    struct Proposal{
+        uint256 nftTokenId;
+        uint256 deadline;
+        uint256 yayVotes;
+        uint256 nayVotes;
+        bool executed;
+        mapping(uint256=>bool) voters;
+    }
+
+    mapping(uint256 => Proposal) public proposals;
+    uint256 public numProposals;
+
+    IFakeNFTMarketplace nftMarketplace;
+    ICryptoDevsNFT cryptoDevsNFT;
+
+    constructor(address _nftMarketplace,address _cryptoDevsNFT) payable {
+        nftMarketplace=IFakeNFTMarketplace(_nftMarketplace);
+        cryptoDevsNFT=ICryptoDevsNFT(_cryptoDevsNFT);
+    }
+
+    modifier nftHolderOnly(){
+        reuire(cryptoDevsNFT.balanceOf(msg.sender)>0,"NOT_A_DAO_MEMBER");
+        _;
+    }
+
+    function createProposal(uint256 _nftTokenId) external nftHolderOnly return (uint256){
+        require(nftMarketplace.available(_nftTokenId),"NFT_NOT_FOR_SALE");
+        Proposal storage proposal=propsals[numProposals];
+        proposal.nftTokenId=_nftTokenId;
+        proposal.deadline=block.timestamp +5 minutes;
+
+        numProposals++;
+        return numProposal-1 
+    }
+
 }

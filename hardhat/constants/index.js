4c5432b09127639acc60de3803b1db2d2136e75d
ranjankjha17 <talk2ranjanktr@gmail.com>
2022-06-02 10:42:59 +0530

    deploy contract

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,5 +1,4 @@
-// Replace the value with your NFT contract address
 const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
-  "YOUR_CRYPTODEVS_NFT_CONTRACT_ADDRESS_HERE";
+  "0x6805a66179283Fa1FcEc8D89f4e162bebe5C0617";
 
 module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,4 +1,36 @@
-const {ethers}=require("hardhat");
+const { ethers } = require("hardhat");
 const { CRYPTODEVS_NFT_CONTRACT_ADDRESS } = require("../constants");
 
+async function main() {
+  // Deploy the FakeNFTMarketplace contract first
+  const FakeNFTMarketplace = await ethers.getContractFactory(
+    "FakeNFTMarketplace"
+  );
+  const fakeNftMarketplace = await FakeNFTMarketplace.deploy();
+  await fakeNftMarketplace.deployed();
 
+  console.log("FakeNFTMarketplace deployed to: ", fakeNftMarketplace.address);
+
+  // Now deploy the CryptoDevsDAO contract
+  const CryptoDevsDAO = await ethers.getContractFactory("CryptoDevsDAO");
+  const cryptoDevsDAO = await CryptoDevsDAO.deploy(
+    fakeNftMarketplace.address,
+    CRYPTODEVS_NFT_CONTRACT_ADDRESS,
+    {
+      value: ethers.utils.parseEther("0.1"),
+    }
+  );
+  await cryptoDevsDAO.deployed();
+
+  console.log("CryptoDevsDAO deployed to: ", cryptoDevsDAO.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+  
+  //FakeNFTMarketplace deployed to:  0x14C11fA0e46141cc176FFe90cc594B2d6234C956
+  //CryptoDevsDAO deployed to:  0x0C1555753Dfc5b4f1892ff390F7E1814f142F1a1
